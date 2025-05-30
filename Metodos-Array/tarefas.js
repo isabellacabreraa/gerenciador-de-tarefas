2ab615c4c14b373b5e273a0510e93dffb1e87fde
Isabella Cabrera <isabella.cabrera@aluno.senai.br>
2025-05-30 10:43:58 -0300

    fix: Corrige erros nos comandos

--- Metodos-Array/tarefas.js
+++ Metodos-Array/tarefas.js
@@ -1,22 +1,22 @@
 let tarefas = ["Estudar","Colocar comida pro Thor","Ler meu livro","Arrumar o quarto","Lavar a louça","Ir à academia","Estudar para concurso","Fazer revisão para o teste","Fazer exercícios de matemática","Comprar mercado"];
 
-console.log(`Antes do push: ${tarefas}
-    `);
+console.log("Antes do push");
+console.table(tarefas);
 
 //Adiciona novas tarefas ao array 
 
 tarefas.push("Varrer a casa","Jogar o lixo fora");
 
-console.log(`Depois do push: ${tarefas}
-    `);
+console.log("Depois do push:");
+console.table(tarefas);
 
 //Removendo a última tarefa do array
 
 let tarefaRemovida = tarefas.pop();
 
 
-console.log(`Depois do pop: ${tarefas}
-    `);
+console.log("Depois do pop:");
+console.table(tarefas);
 console.log(`Tarefa removida: ${tarefaRemovida}
     `);
 
@@ -24,15 +24,15 @@ console.log(`Tarefa removida: ${tarefaRemovida}
 
 tarefas.unshift("Lavar o cabelo");
 
-console.log(`Depois do unshift: ${tarefas}
-    `);
+console.log("Depois do unshift:");
+console.table(tarefas);
 
 //Removendo a primeira tarefa do array 
 
 let tarefaRemovida01= tarefas.shift();
 
-console.log(`Depois do shift: ${tarefas}
-    `);
+console.log("Depois do shift:");
+console.table(tarefas);
 
 console.log(`Tarefa removida: ${tarefaRemovida01}
     `);
