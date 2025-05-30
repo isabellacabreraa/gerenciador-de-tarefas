f5492066386387cb130607b07d0d6a37a34439a3
Isabella Cabrera <isabella.cabrera@aluno.senai.br>
2025-05-30 09:41:46 -0300

    feat: Adiciona tarefa no início e altera o arquivo README.md

--- Metodos-Array/tarefas.js
+++ Metodos-Array/tarefas.js
@@ -1,17 +1,29 @@
 let tarefas = ["Estudar","Colocar comida pro Thor","Ler meu livro","Arrumar o quarto","Lavar a louça","Ir à academia","Estudar para concurso","Fazer revisão para o teste","Fazer exercícios de matemática","Comprar mercado"];
 
-console.log(`Antes do push: ${tarefas}`);
+console.log(`Antes do push: ${tarefas}
+    `);
 
 //Adiciona novas tarefas ao array 
 
 tarefas.push("Varrer a casa","Jogar o lixo fora");
 
-console.log(`Depois do push: ${tarefas}`);
+console.log(`Depois do push: ${tarefas}
+    `);
 
 //Removendo a última tarefa do array
 
 let tarefaRemovida = tarefas.pop();
 
 
-console.log(`Depois do pop: ${tarefas}`);
-console.log(`Tarefa removida: ${tarefaRemovida}`);
+console.log(`Depois do pop: ${tarefas}
+    `);
+console.log(`Tarefa removida: ${tarefaRemovida}
+    `);
+
+//Adicionando uma nova tarefa no início do array
+
+tarefas.unshift("Lavar o cabelo");
+
+console.log(`Depois do unshift: ${tarefas}
+    `);
+
